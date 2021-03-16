2c699d27cb1a0e03d97fff0df06caa116a03bca0
jose <macaverde604@gmail.com>
2021-03-16 15:45:09 -0300

    adiciona figura p relatorio

--- view.R
+++ view.R
@@ -1,19 +1,5 @@
 rm(list=ls())
 
-# read data
-data = read.csv(file = 'dados/trainReduzido.csv')
-valida = read.csv(file = 'dados/validacao.csv')
-
-# label, id
-id.label = as.matrix(data[, 1:2])
-id.valida = as.matrix(valida[, 1])
-
-# clusters
-cluster.1 = valida[1:1000, ]
-cluster.2 = valida[1001:2000, ]
-cluster.3 = valida[2001:3000, ]
-cluster.4 = valida[3001:4000, ]
-
 # view
 MostraImagem = function(x, main = 'imagem', n = 28){
   rotate = function(x) t(apply(x, 2, rev))
@@ -28,10 +14,30 @@ matriz = function(x, n = 28){
   return(matrix(img, nrow = n * n))
 }
 
-N = 100
-for(rep in seq(N)){
-  # random = floor(runif(n = 1, min = 1, max = 4000))
-  i = rep + 3852
-  amostra = matriz(as.matrix(valida[i, -1]))
-  MostraImagem(amostra, main = paste('imagem', i))
-}
+# read data
+data = read.csv(file = 'dados/trainReduzido.csv')
+valida = read.csv(file = 'dados/validacao.csv')
+
+# label, id
+id.label = as.matrix(data[, 1:2])
+id.valida = as.matrix(valida[, 1])
+
+# clusters
+cluster.1 = valida[1:1000, ]
+cluster.2 = valida[1001:2000, ]
+cluster.3 = valida[2001:3000, ]
+cluster.4 = valida[3001:4000, ]
+
+# single
+i = 4205
+amostra = matriz(as.matrix(data[i, -(1:2)]))
+MostraImagem(amostra, main = paste('Imagem ', i))
+
+# # varredura
+# N = 100
+# for(rep in seq(N)){
+#   # random = floor(runif(n = 1, min = 1, max = 4000))
+#   i = rep + 3852
+#   amostra = matriz(as.matrix(valida[i, -1]))
+#   MostraImagem(amostra, main = paste('imagem', i))
+# }
