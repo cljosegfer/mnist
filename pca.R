e926089d895c692acaa33f4beff53890e2dbcbff
jose <macaverde604@gmail.com>
2021-03-17 23:42:43 -0300

    substitui Rdata por rds

--- pca.R
+++ pca.R
@@ -4,18 +4,18 @@ pca = function(data, file){
   X = data.matrix(data[, -(1:2)])
   pca = prcomp(X)
   data.pca = pca$x
-  path = paste('data/', file, '-pca.Rdata', sep = '')
-  save(data.pca, file = path, compress = FALSE)
+  path = paste('data/', file, '-pca.rds', sep = '')
+  saveRDS(data.pca, file = path, compress = FALSE)
 }
 
 # read data
 data = read.csv(file = 'dados/trainReduzido.csv')
 valida = read.csv(file = 'dados/validacao.csv')
 
-# pca
-pca(data, file = 'mnist')
-pca(valida, file = 'valida')
+# # pca
+# pca(data, file = 'mnist')
+# pca(valida, file = 'valida')
 
-# # test
-# load(file = 'data/mnist-pca.Rdata')
-# load(file = 'data/valida-pca.Rdata')
+# test
+data.pca = readRDS(file = 'data/mnist-pca.rds')
+valida.pca = readRDS(file = 'data/valida-pca.rds')
